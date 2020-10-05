7c988a5fbf02c0b9eba8664520c22f35030808eb
Matheus Santos <matheus_sa_santos@hotmail.com>
2020-10-05 19:14:38 -0300

    delete all user from heroku

--- src/database/seeds/index.ts
+++ src/database/seeds/index.ts
@@ -5,8 +5,15 @@ import User from '../../models/User';
 
 
 createConnection().then(async connection => {
+  // Delete all users
+  await getConnection()
+    .createQueryBuilder()
+    .delete()
+    .from(User)
+    .where("username = :name", { name: "Admin" })
+    .execute();
 
-  // // CREATE INITIAL ROLES
+  // CREATE INITIAL ROLES
   // await getConnection()
   //   .createQueryBuilder()
   //   .insert()
@@ -27,11 +34,11 @@ createConnection().then(async connection => {
   //   ])
   //   .execute();
 
-  const user = await getConnection().createQueryBuilder().select("user")
-    .from(User, "user").where("user.name = :name", { name: "Admin" }).getOne();
+  // const user = await getConnection().createQueryBuilder().select("user")
+  //   .from(User, "user").where("user.name = :name", { name: "Admin" }).getOne();
 
-  const roles = await getConnection().createQueryBuilder().select("roles")
-    .from(Role, "roles").where("roles.name = :name", { name: "ROLE_ADMIN" }).getOne();
+  // const roles = await getConnection().createQueryBuilder().select("roles")
+  //   .from(Role, "roles").where("roles.name = :name", { name: "ROLE_ADMIN" }).getOne();
 
 
   // CREATE A RELATION BETWEEN ROLES AND USER FOR US HAVE ACCESS TO PROJECT
@@ -41,11 +48,12 @@ createConnection().then(async connection => {
   //   .of(user?.id)
   //   .add(roles?.id);
 
-  await connection
-    .createQueryBuilder()
-    .relation(User, "roles")
-    .of(user?.id) // posts
-    .remove(roles?.id); // image
+  // DELETE THE DATA FROM TABLE USERS_ROLES 
+  // await connection
+  //   .createQueryBuilder()
+  //   .relation(User, "roles")
+  //   .of(user?.id) // users
+  //   .remove(roles?.id); // roles
 
 
 }).catch(error => console.log(error));
